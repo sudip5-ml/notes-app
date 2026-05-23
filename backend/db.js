20838b3d7c56d505263e29781329726647ad881a
Sudip Neupane <neupanesudip388@email.com>
2026-05-23 08:44:08 +0545

    setup MySQL database and backend connection

---
+++ backend/db.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const mysql = require('mysql2')
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+})
+
+db.connect((err) => {
+  if (err) {
+    console.log('Database connection failed!', err)
+  } else {
+    console.log('Database connected successfully!')
+  }
+})
+
+module.exports = db
