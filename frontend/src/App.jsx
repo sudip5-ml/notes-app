230a6cc99ad724d0eeccf63b98718d93bed9b128
Sudip Neupane <neupanesudip388@email.com>
2026-06-06 09:55:59 +0545

    Save local changes before pull

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
 import Dashboard from './pages/dashboard'
 
 function App() {
@@ -9,8 +7,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
